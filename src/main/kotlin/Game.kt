2497921160e312d1c3bdb616e6583c2346826d1d
Jereme Causing <jeremejazz15@gmail.com>
2026-01-08 11:03:41 +0800

    initialize project

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,48 @@
+package me.jereme
+
+class Game {
+    private val board = MutableList<Cell>(size=9){ Cell.Empty }
+    private var status: Status= Status.Idle
+    private lateinit var player: Player
+
+    fun start() {
+        status = Status.Running
+        println(" ------------------------- ")
+        println("| Welcome to TIC-TAC-TOE |")
+        println("| Pick a number from 1-9 |")
+        println(" ------------------------ ")
+    }
+
+    private fun getName(){
+        print("Choose Your Name: ")
+        val name = readlnOrNull()
+        try{
+            require(value = name != null)
+            player = Player(name = name, symbol = 'X')
+            println("It's your move, $name")
+        }catch(e: Throwable){
+            println(e.message)
+        }
+    }
+}
+
+data class Player(
+    val name: String = "Computer",
+    val symbol: Char = 'O'
+    )
+
+
+sealed class Status{
+    object Idle: Status()
+    object Running: Status()
+    object GameOver: Status()
+}
+
+sealed class Cell(val placeholder: Char){
+    object Empty: Cell(placeholder = '_')
+    data class Filled(val player: Player): Cell(placeholder = player.symbol)
+}
+
+
+
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package me.jereme
+
+fun main() {
+    Game().start()
+}
